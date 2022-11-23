c3e0e223b7ca8dd21a0cc610373d59be550e3301
tyron <tyron.ambroisine@gmail.com>
2022-11-23 21:07:57 +0100

    Update Affichage and add navire class

--- src/Affichage.java
+++ src/Affichage.java
@@ -5,26 +5,38 @@
 
 public class Affichage {
 
-    public class Grid1 {
-        public char [][] grid = new char [10][10];
+    public static class Grid1 {
+        int x = 16;
+        int y = 16;
+        public String [][] grid = new String[x][y];
         public Grid1() {
+            int n = 0;
             //initialize grid
-            for(int outerLoopValue = 0; outerLoopValue<10;outerLoopValue++)
+            for(int innerLoopValue = 0; innerLoopValue<x;innerLoopValue++)
             {
-                for(int innerLoopValue = 0; innerLoopValue<10;innerLoopValue++)
+                grid[0][innerLoopValue]= "| "+ n +"|";
+                n++;
+            }
+            for(int outerLoopValue = 0; outerLoopValue<y;outerLoopValue++)
+            {
+                for(int innerLoopValue = 0; innerLoopValue<x;innerLoopValue++)
                 {
-                    grid[outerLoopValue][innerLoopValue]='O';
+                    grid[outerLoopValue][innerLoopValue]= "|__";
                 }
             }
         }
         public void PrintGrid() {
-            for(int outerLoopValue = 0; outerLoopValue<10;outerLoopValue++)
+            char ch = 'a';
+            for(int outerLoopValue = 0; outerLoopValue<y;outerLoopValue++)
             {
-                System.out.println("");
-                for(int innerLoopValue = 0; innerLoopValue<10;innerLoopValue++)
+                System.out.println();
+                System.out.print(ch);
+                ch++;
+                for(int innerLoopValue = 0; innerLoopValue<x;innerLoopValue++)
                 {
-                    System.out.print(grid[outerLoopValue][innerLoopValue]+"  ");
+                    System.out.print(grid[outerLoopValue][innerLoopValue]);
                 }
+                System.out.print("|");
             }
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ public class Main {
     {
         System.out.println("Hello world!");
         System.out.println("test");
+        Affichage.Grid1 CPUGrid = new Affichage.Grid1();
+        Affichage.Grid1 PGrid = new Affichage.Grid1();
+
+        System.out.println("Welcome to Battleships");
+        PGrid.PrintGrid();
+
 
     }
 
