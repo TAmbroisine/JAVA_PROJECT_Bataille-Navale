d89e609dd238b98d7fcdf8638befc9cf87f3b017
yannis <yannis.ach2002@gmail.com>
2022-11-23 21:35:03 +0100

    Update add method in different boat

--- src/Cuirasse.java
+++ src/Cuirasse.java
@@ -1,4 +1,20 @@
 public class Cuirasse extends Navire
 {
+    @Override
+    public void Deplacement()
+    {
 
+    }
+
+    @Override
+    public void Tir()
+    {
+
+    }
+
+    @Override
+    public void Positionnement()
+    {
+
+    }
 }
--- src/Sous_Marin.java
+++ src/Sous_Marin.java
@@ -1,4 +1,20 @@
 public class Sous_Marin extends Navire
 {
+    @Override
+    public void Deplacement()
+    {
 
+    }
+
+    @Override
+    public void Tir()
+    {
+
+    }
+
+    @Override
+    public void Positionnement()
+    {
+
+    }
 }
