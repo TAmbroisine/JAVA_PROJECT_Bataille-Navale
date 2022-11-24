225d088a8677be52cd582053f1cb83a207e8ae8a
T.AMBROISINE <tyron.ambroisine@gmail.com>
2022-11-24 08:27:25 +0100

    test

--- src/Navire.java
+++ src/Navire.java
@@ -3,8 +3,7 @@ abstract class Navire
     //No need for Deplacement to be abstract because it works the same for Every ship
     //Pas besoin pour Deplacement d'être abstrait car il fonctionne de la même manière pour tout les bateaux
     public void Deplacement (){
-        
-        int x = 1;
+
     }
     abstract public void Tir (int x, int y);
     abstract public void Positionnement ();
