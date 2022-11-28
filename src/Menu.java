b154b7588e62413ca57402fdb7189adde4b786cc
tyron <tyron.ambroisine@gmail.com>
2022-11-28 18:26:30 +0100

    Changes by yanni

--- src/Menu.java
+++ src/Menu.java
@@ -21,14 +21,49 @@ public class Menu extends Affichage
                 Affichage.Grid1 CPUGrid = new Affichage.Grid1();
                 Affichage.Grid1 PGrid = new Affichage.Grid1();
                 PGrid.PrintGrid();
-
-
                 break;
             case 2 :
+                //Pour charger une partie
                 break;
             case 3 :
+                //Pour afficher les aides
+                System.out.println("Pour afficher les règles : tapez 1");
+                System.out.println("Pour afficher les touches : tapez 2");
+                System.out.println("Pour sortir du menu : tapez Un autre chiffre ");
+                do
+                {
+                    switch (userReader.nextInt())
+                    {
+                        case 1 :
+                            //règle du jeu
+                            System.out.println("Bienvenue dans les règles : \n"+
+                                    "\n" +
+                                    "En début de jeu les navires sont positionnés aléatoirement sur la grille, Votre but est de détruire tous les navires adverses ! \n" +
+                                    "Petites particularitées : \n"+
+                                    "LES BATEAUX NAVIGUE PENDANT LA PARTIE \n" +
+                                    "\n"+
+                                    "Pour TIRER : \n"+
+                                    "LE PREMIER TIR DUN DESTROYER DEVOILE UN CARRE DE 4x4 A PARTIR DU COIN HAUT ET GAUCHE \n "+
+                                    "SEUL LE SOUS MARIN PEUt TOUCHER ET COULER LE SOUS MARIN ADVERSE \n " +
+                                    "POUR COULER UN NAVIRE, IL FAUT AVOIR TOUCHER TOUTES LES CASES QU’IL OCCUPE \n" +
+                                    "UN CUIRASSE EST PLUS RESISTANT QU’UN SIMPLE DESTROYER \n" +
+                                    "DES QUE LE NAVIRES EST COULE, LE NAVIRE ADVERSE DISPARAIT ET LA CASE TOUCHE SAFFICHE");
+                            break;
+                        case 2 :
+                            //touches du jeu
+                            System.out.println("Bienvenue dans les touches : \n"+
+                                    "\n" +
+                                    "");
+                            break;
+                        default :
+                            System.out.println("saisissez un chiffre autre que 1 ou 2 pour sortir");
+ // [1] : Affiche les règle
+ // [2] : Affiche les touches
+                    }
+                }while (userReader.nextInt() == 2 | userReader.nextInt() == 1 );
                 break;
             case 4 :
+                //Pour quitter la partie
                 System.out.println("cls");
                 System.out.println("Vous avez quitté la partie");
                 break;
