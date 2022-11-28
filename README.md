1d1e09efc733b43597199d70ca01fef394d6556a
tyron <tyron.ambroisine@gmail.com>
2022-11-28 18:26:22 +0100

    Changes by theworm

--- README.md
+++ README.md
@@ -7,17 +7,23 @@
 
 ## Etape 2: Mouvement
     - placement navires
+    - Sélection navires
     - Mouvement navires
         - w/ dégats reçus
         - w/o dégats reçus
 
 ## Etape 3: Dégats
     - Tir navires
+        - mod coordonnées
+        - mod curseur consol
     - Tir personalisé pour chaque navires
     - affichage dégats navires
 
 ## Etape 4: Ordinateur
     - Notion d'intelligence de l' ordinateur
+        - aléatoire débile
+        -  aléatoire réduite autour des bateaux du joueurs
+        - implémentation de l'IA A* (avec calcul de probabilité)
 
 ## Progrès Global
 
--- src/Affichage.java
+++ src/Affichage.java
@@ -10,8 +10,8 @@ public class Affichage {
     Affichage(){}
 
     public static class Grid1 {
-        int x = 16*2;
-        int y = 16;
+        int x = 27*2;
+        int y = x/2;
 
         int separateur = x/2 + 1;
         public String [][] grid = new String[x+1][y];
@@ -93,7 +93,7 @@ public class Affichage {
                     System.out.print("|"+ n);
                 }
                 n++;
-                if(n>15 && reset){
+                if(n>(x/2)-1 && reset){
 
                     System.out.print("|   ");
                     n=0;
@@ -104,11 +104,24 @@ public class Affichage {
         }
      public void Nom_Grille(){
          System.out.println();
-         System.out.print("                   ");
+         for(int i = 0; i < (x/4)-2; i++){
+             System.out.print("   ");
+         }
          System.out.print("Grille du joueur");
-         System.out.print("                   ");
-         System.out.print("       ");
-         System.out.println("Grille des dégats sur l'ordinateur");
+         for(int i = 0; i < (x/4)+3-5; i++) { // +4 pour prendre en compte l'espacement
+             System.out.print("   ");
+         }
+         //System.out.print("a");
+
+         for(int i = 0; i < (x/4)-5; i++){
+             System.out.print("   ");
+         }
+         System.out.print("Grille des dégats sur l'ordinateur");
+         for(int i = 0; i < (x/4)-1; i++) {
+             System.out.print("   ");
+         }
+         System.out.println("");
+
      }
 
     }
